d46d38338a16d986b9ddd44970c7d7ebc381b947
indvd00m <gotoindvdum@gmail.com>
2016-11-22 20:40:12 +0300

    Lang fix

--- lang/en.js
+++ lang/en.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.setLang("crossreference","en",{
 	'name': 'Reference to manual',
 	'description': 'Cross-reference link to manual',
-	"filter":"Filter",
+	"filter":"Search / filter",
 	"manual":"Manual"
 });
